6dde6648e5bb13e414b2184867788cf58b6d9c10
nathan <2243151@cegepmontpetit.ca>
2023-08-29 14:31:17 -0400

    Don't Ask too much question

---
+++ triangle/src/main/java/org/example/App.java
@@ -0,0 +1,62 @@
+package org.example;
+
+
+import org.jsoup.Jsoup;
+import  org.jsoup.helper.ValidationException;
+
+import  org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.print.Doc;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       /* Random r = new Random();
+     int o=   r.nextInt(100);
+        int l=   r.nextInt(100);
+        int[] a= {l,o};
+      int min=  Arrays.stream(a).min().getAsInt();
+      int max = Arrays.stream(a).max().getAsInt();
+      int sum = Arrays.stream(a).sum();
+      double avg = Arrays.stream(a).average().getAsDouble();
+      System.out.println(min);
+        System.out.println(max);
+        System.out.println(sum);
+        System.out.println(avg);
+
+       try{
+           Document doc = Jsoup.connect("").get();
+           Elements e = doc.select("html");
+           System.out.println(e.toString());
+        }
+       catch (Exception e) {
+           System.err.println("aint no way");
+
+       }
+
+            String pomme ="Pomme";
+            String NewPomme = "";
+        for (int i = 0; i <pomme.length() ; i++) {
+            NewPomme += pomme.charAt(i)+ "\n";
+
+        }System.out.print(NewPomme);
+        */
+
+
+
+    }
+
+}
